baaa502e9c8e63abfa1bf822a106fe54b7bfe05b
Benedek Papp <b.papp@warwick.ac.uk>
2020-08-09 20:09:43 +0100

    Overhauled again, but now the system works although howswap is buggy

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Fully decentralised website system.
 
+# All of this is now outdated, needs to be updated, with some better explanation
+
 ## Plan:
 1. Point .eth address to handler.index
 2. Handler shows splashscreen.
@@ -13,13 +15,17 @@
 Add the folder containing the website to IPFS, call the hash of this folder `HASH_SITE`
 Add the folder containing the packages to IPFS, call the hash of the folder `HASH_PACKAGES`
 Add the folder containing the handler to IPFS, call the hash of the folder `HASH_HANDLER`
+Set the URL in the pointer file to `~~~some gateway~~~/ipfs/HASH_POINTER`
+Add the pointer file to IPFS and let the hash be `HASH_POINTER`
+
+Do the last 3 each time you update the site, and choose if 'Setting ENS' or 'Setting IPNS' is appropriate.
 
 ### Setting up ENS (.eth):
 Acquire an .eth domain, let this be `x.eth`.
 Create a set of major version domains, for example `major_a.x.eth` and `major_b.x.eth`. Two are recommended, as when a new major version is being added to one, the other needs to be available as a fallback containing the previous version.
 Major versions will be loaded near instantly. But require an ethereum transaction to update, meaning a small cost.
 
-Set the content entry for `x.eth` to `HASH_HANDLER`
+Set the content entry for `x.eth` to `HASH_POINTER`
 For `major_a.x.eth` and `major_b.x.eth` set the content entry on one to `HASH_SITE`, when you want to publish a new major version, set the new `HASH_SITE` on alternating subdomains. So one always points to the newest `HASH_SITE` and the other to the one major version out-of-date `HASH_SITE`
 
 ### Setting up IPNS
--- handler/handler.js
+++ handler/handler.js
@@ -6,80 +6,118 @@ site_screen = document.getElementById("site_screen")
 loading_screen.style.display = 'block'
 site_screen.style.display = 'none'
 
-major_subdomain_a = "/ipfs/QmWRHL3V4JFhf3oUnzAfdQG9wQrR1PjWVQBAsieinJtH4w"
-major_subdomain_b = "/ipfs/QmcuBhbRbBKThFaASYYyaD8U494uLabnPGMPhwQk8NiJd7"
+minor_a_IPNS = "/ipns/QmZU8fJWrEKfwmhMFmDEt5bThKX7Kz6QxJQP9AZ2E7SHnC"
+minor_b_IPNS = "/ipns/Qmaf23Aknagf5Fkc2oNbvCvfGYbkDpZVZqD2RD9cZ2ZuRZ"
+minors = [minor_a_IPNS,minor_b_IPNS]
 
 function show_loading(){
-    loading_screen.style.display = 'block'
-    site_screen.style.display = 'none'
-    loading_screen.classList.add('d-flex')
+  console.log("Showing Loading Screen")
+  loading_screen.style.display = 'block'
+  site_screen.style.display = 'none'
+  loading_screen.classList.add('d-flex')
+  is_loading = true
 }
 
 function hide_loading(){
-    loading_screen.style.display = 'none'
-    loading_screen.classList.remove('d-flex')
-    site_screen.style.display = 'block'
+  console.log("Hiding Loading Screen")
+  loading_screen.style.display = 'none'
+  loading_screen.classList.remove('d-flex')
+  site_screen.style.display = 'block'
+  is_loading = false
 }
 
 function resized(){
-    if(is_loading){
-        show_loading()
-    }else{
-        hide_loading()
-    }
+  console.log("Resize was called")
+  if(is_loading){
+    show_loading()
+  }else{
+    hide_loading()
+  }
 }
 
-checked_major_a = false
-checked_major_b = false
-major_a_version = -1.0
-major_b_version = -1.0
+minor_a_version = -1.0
+minor_b_version = -1.0
 
+//THINGS TO SET
+version_loaded = 0
+iframe = document.getElementById("iframe").src = "/ipfs/QmTH5Rw6kBsAJgbGySiJeYaqh83hA5V9pavjQz3GVPwU8o" //Put hash of most recent major site version here
+hide_loading()
 
+url_to_load = ""
 
-function load_latest_major(){
-    fetch(major_subdomain_a+"/version.txt")
+function load_latest(url_a,url_b){
+  console.log("Started looking at update IPNSs")
+  checked_a = false
+  checked_b = false
+  
+  fetch(url_a+"/version.txt")
     .then(function(response) {
       response.text().then(function(text) {
-        
         done_a(text);
-      });
     });
+  });
   
   function done_a(text) {
     temp_str = text.split('\n')[0]
-    temp_str = temp_str.substr(7,temp_str.length)
-    major_a_version = parseFloat(temp_str)
-    checked_major_a = true
+    temp_str = temp_str.substr(2)
+    minor_a_version = parseFloat(temp_str)
+    console.log("Checked path A, got V:"+minor_a_version)
+    checked_a = true
     load_latest()
   }
 
-  fetch(major_subdomain_b+"/version.txt")
+  fetch(url_b+"/version.txt")
     .then(function(response) {
       response.text().then(function(text) {
         done_b(text)
-      });
     });
+  });
   
   function done_b(text) {
     temp_str = text.split('\n')[0]
-    temp_str = temp_str.substr(7,temp_str.length)
-    major_b_version = parseFloat(temp_str)
-    checked_major_b = true
+    temp_str = temp_str.substr(2)
+    minor_b_version = parseFloat(temp_str)
+    console.log("Checked path B, got V:"+minor_b_version)
+    checked_b = true
     load_latest()
   }
-
-  iframe = document.getElementById("iframe")
+  
   function load_latest(){
-      if (checked_major_b && checked_major_a){
-          if (major_a_version>major_b_version){
-              iframe.src = major_subdomain_a
-              hide_loading()
-          } else{
-              iframe.src = major_subdomain_b
-              hide_loading()
-          }
-      }
+    iframe = document.getElementById("iframe")
+    if (checked_b && checked_a){
+      console.log("Both pathes have been checked")
+      if(minor_b_version>minor_a_version && minor_a_version > version_loaded){
+        console.log("Chose path b")
+        url_to_load = url_b
+        version_loaded = minor_b_version
+        send_to_url()
+      }else if(minor_a_version>minor_b_version && minor_b_version>version_loaded){
+        console.log("Chose path a")
+        url_to_load = url_a
+        version_loaded = minor_a_version
+        send_to_url()
+      }else if(minor_a_version === version_loaded || minor_b_version === version_loaded){
+        console.log("No updates were found")
+      }else{
+        console.log("Something went wrong with finding the newest version.")
+      }          
+    }  
   }
+
+  function send_to_url(){
+    console.log("Navigating to address")
+    iframe.src = url_to_load
+    hide_loading()
+  }
+
 }
 
-load_latest_major()
+load_latest(minors[0],minors[1])
+
+//Keep checking for new minor release
+setInterval(function(){
+  console.log("Update interval fired")
+  load_latest(minors[0],minors[1]);
+ },10000);
+
+//NEED TO IMPLEMENT TIMEOUT FOR CHECKING THE IPNS ADDRESSES. OTHERWISE HOTSWAP HANGS
